e9d95d45bcc42319c08288b22c46e5ad47f3b5d8
winglessOracle <carlo@wesseling.io>
2023-03-21 12:39:47 +0100

    structure

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 10:03:07 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/03/21 10:04:02 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/03/21 12:37:16 by cwesseli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,51 @@
 
 # include "../libft/libft.h"
 # include <errno.h>
-# include <stdbool.h>
-# include <limits.h>
+// # include <stdbool.h>
+// # include <limits.h>
 # include <stdio.h>
+
+typedef struct s_node
+{
+	int				type;
+	char			*content;
+	struct s_node	*prev;
+	struct s_node	*next;
+}	t_node;
+
+typedef enum e_commandtype
+{
+	STD = 0,
+	BUILT_IN,
+}	t_commandtype;
+
+typedef enum e_lextype
+{
+	WORD = 0,
+	GREAT,
+	LESS,
+	G_GREAT,
+	L_LESS,
+	PIPE,
+	VAR,
+	OPERATOR,
+	ASSIGN,
+	D_QUOTE,
+	S_QUOTE,
+	WILD_CARD,
+	AND_AND,
+	OR_OR,
+}	t_lextype;
+
+typedef struct s_master
+{
+	char	*infile;
+	char	*outfile;
+	char	*errorfile;
+	char	**local_vars;
+	t_node	**commands;
+	
+}	t_master;
 
 
 
