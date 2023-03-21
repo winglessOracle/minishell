e75dabddad8d285b269a16eb4384334e520344ae
winglessOracle <carlo@wesseling.io>
2023-03-21 12:47:19 +0100

    structure

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,14 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 10:03:07 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/03/21 12:37:16 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/03/21 12:46:20 by cwesseli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "../libft/libft.h"
+# include "libft.h"
 # include <errno.h>
 // # include <stdbool.h>
 // # include <limits.h>
