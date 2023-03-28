4573cc10f8aa442d648b97aaee52c622f19e66f8
ccaljouw <ccaljouw@student.codam.nl>
2023-03-28 09:22:45 +0200

    uncommented test

--- src/test.c
+++ src/test.c
@@ -6,7 +6,7 @@
 /*   By: ccaljouw <ccaljouw@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/24 13:08:03 by ccaljouw      #+#    #+#                 */
-/*   Updated: 2023/03/27 23:29:21 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/03/28 09:21:56 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,5 @@ void	test_lexer(char *str, t_node **tokens)
 void	run_tests(char *str, t_node **tokens, t_master *master)
 {
 	test_lexer(str, tokens);
-	// print_env(master->env_list);
+	print_env(master->env_list);
 }
