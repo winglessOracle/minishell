e19ffff0692f4c2873ef90e0ecf681c68f22cffc
WinglessOracle <carlo@wesseling.io>
2023-03-30 08:53:32 +0200

    b

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 10:03:07 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/03/29 10:15:56 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/03/30 08:52:01 by carlo         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
