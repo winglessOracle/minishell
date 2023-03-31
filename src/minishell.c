81508f55ae5561cc0a9fc9c129a2cc155f977e2d
winglessOracle <carlo@wesseling.io>
2023-03-31 10:18:41 +0200

    workign version at codam

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 09:48:38 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/03/29 11:29:16 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/03/31 10:18:19 by cwesseli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,18 @@ int	main(int argc, char **argv, char **envp) //remove arguments and return type?
 	line_read = NULL;
 	master = init_master_struct();
 	master->env_list = env_to_list(envp);
-	add_variable(master->env_list, "PS1=CC_PROMPT:> ", 1);
+	add_variable(master->env_list, "PS1=CC_PROMPT:>", 1);
 	add_variable(master->env_list, "PS2=", 1);
 	while (1)
 	{
+		//add siganal initializer add flag restart for certain strings
+		
 		if (line_read)
 		{
 		  	free(line_read);
 		  	line_read = NULL;
 		}
-		rl_prompt = get_variable(master->env_list, "PS1");
-		line_read = readline(rl_prompt);
+		line_read = readline(get_variable(master->env_list, "PS1"));
 		if (!ft_strcmp(line_read, "exit"))
 			break ;
 		if (line_read && *line_read)
@@ -44,8 +45,8 @@ int	main(int argc, char **argv, char **envp) //remove arguments and return type?
 		// tests
 		run_tests(line_read, tokens, master);
 	
-		if (tokens)
-			lstclear(&tokens, delete_content);
+		// if (tokens)
+		//	lstclear(&tokens, delete_content);
 	}
 	//clear_history(); should be rl_clear_history() but that is not found?.
 
