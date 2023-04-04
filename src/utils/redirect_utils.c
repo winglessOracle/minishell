28bf75f0e2bfba16c3a04e36598d9b7a8d5a289d
ccaljouw <ccaljouw@student.codam.nl>
2023-04-04 10:15:02 +0200

    fixed variable not used in redirect

--- src/utils/redirect_utils.c
+++ src/utils/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: cariencaljouw <cariencaljouw@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/30 15:56:14 by cariencaljo   #+#    #+#                 */
-/*   Updated: 2023/04/04 09:34:05 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/04/04 10:14:05 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 #include <unistd.h>
 
 int	redirect_input(t_node **token, t_smpl_cmd *cmd)
-{
-	// int	state;
-	
-	// state = remove_node(token, cmd);
+{	
+	remove_node(token, cmd);
 	while ((*token)->type == _SPACE || (*token)->type == _TAB || \
 		(*token)->type == COMMENT)
-		// state = remove_node(token, cmd);
+		remove_node(token, cmd);
 	// if ((*token)->type == ASSIGN || (*token)->type == EXPAND)
 	// 	state = parse[tokens->type](&tokens, *cmd);
 	if (access((*token)->content, R_OK) == -1)
