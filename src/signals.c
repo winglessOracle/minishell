56b76cecf31562328b43ad3585f888a4bd35d45c
winglessOracle <carlo@wesseling.io>
2023-04-04 09:37:51 +0200

    removed ctrl-z signal handeling in favour of sig_ign

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/31 12:30:55 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/04/03 16:50:37 by cwesseli      ########   odam.nl         */
+/*   Updated: 2023/04/04 09:36:43 by cwesseli      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	set_sig_term(void)
 	set_signals();
 }
 
-void	handle_sigcont(int signal_number)
-{
-	(void) signal_number;
-	set_termios();
-}
+// void	handle_sigcont(int signal_number)
+// {
+// 	(void) signal_number;
+// 	set_termios();
+// }
 
 void	handle_sigint(int signal_number)
 {
@@ -41,23 +41,32 @@ void	handle_sigquit(int signal_number)
 void	set_signals(void)
 {
 	struct sigaction	sa_quit;
-	struct sigaction	sa_cont;
+	struct sigaction	sa_stop;
 	struct sigaction	sa_int;
+	//struct sigaction	sa_cont;
 
 	sa_quit.sa_handler = handle_sigquit;
-	sa_quit.sa_flags = SA_RESTART;
+	//sa_quit.sa_flags = SA_RESTART;
 	sigemptyset(&sa_quit.sa_mask);
 	sigaddset(&sa_quit.sa_mask, SIGQUIT);
 	if (sigaction(SIGQUIT, &sa_quit, NULL) == -1)
 		exit(errno);
-	sa_cont.sa_handler = handle_sigcont;
-	sa_cont.sa_flags = 0;
-	if (sigaction(SIGCONT, &sa_cont, NULL) == -1)
+
+	sa_stop.sa_handler = SIG_IGN;
+	sigemptyset(&sa_stop.sa_mask);
+	sigaddset(&sa_stop.sa_mask, SIGTSTP);
+	if (sigaction(SIGTSTP, &sa_stop, NULL) == -1)
 		exit(errno);
+
 	sa_int.sa_handler = handle_sigint;
-	sa_int.sa_flags = 0;
+//	sa_int.sa_flags = 0;
 	sigemptyset(&sa_int.sa_mask);
 	sigaddset(&sa_int.sa_mask, SIGINT);
 	if (sigaction(SIGINT, &sa_int, NULL) == -1)
 		exit(errno);
+		
+	// sa_cont.sa_handler = handle_sigcont;
+	// sa_cont.sa_flags = 0;
+	// if (sigaction(SIGCONT, &sa_cont, NULL) == -1)
+	// 	exit(errno);
 }
