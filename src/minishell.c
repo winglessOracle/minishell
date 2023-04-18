4457498c92f001f3d683a6fe836baaaf1ae13fcd
ccaljouw <ccaljouw@student.codam.nl>
2023-04-18 10:16:35 +0200

    new errors in segfault.txt. pargins does not work anymore for a lot of cases

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: cwesseli <cwesseli@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/21 09:48:38 by cwesseli      #+#    #+#                 */
-/*   Updated: 2023/04/18 09:44:11 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/04/18 10:15:51 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
