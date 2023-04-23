edcf432a4be8260debdb4a40fa6c49b84ce8afd9
WinglessOracle <carlo@wesseling.io>
2023-04-23 02:02:22 +0200

    refactored tester

--- tester.sh
+++ tester.sh
@@ -22,290 +22,168 @@ trap 'rm -rf ./test' EXIT
 
 echo -e "\n\033[1m\033[38;5;202mTesting Minishell vs Bash...\033[0m\n"
 
+compare_output() {
+
+## Compare outputs and exit codes
+
+	echo -e "\tComparing output..."
+	diff ./test/bash_output.txt ./test/minishell_output.txt > ./test/diff
+	if [ "$?" -eq "0" ]; then
+    	echo -e "\t\e[32mOutput OK!\e[0m"
+	else
+    	echo -e "\t\e[31mOutput KO!\e[0m\n"
+		echo -e "Delta:"
+		cat ./test/diff
+	fi
+
+	## Compare exit codes
+	echo -e "\tComparing exit codes..."
+	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
+    	echo -e "\t\e[32mExit code OK!\e[0m\n"
+	else
+    	echo -e "\t\e[31mExit code KO!\e[0m"
+    	echo -e "\tExpected exit code: $exitcode_bash"
+    	echo -e "\tFound exit code: $exitcode_minishell\n"
+	fi
+}
+
 single_tests() {
     echo "Running single command tests..."
-	echo -e "\tMinishell commands..."
 
 	## Minishell tests
 	ls > ./test/minishell_output.txt
 	find Makefile >> ./test/minishell_output.txt
+
 	exitcode_minishell=$?
 
  	## Bash tests
-	echo -e "\tRunning Bash commands..."
 	bash -c 'ls; find Makefile' > ./test/bash_output.txt
-	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
 
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	exitcode_bash=$?
+	compare_output
 }
 
 multi_tests() {
     echo "Running multi tests..."
-	echo -e "\tMinishell commands..."
+
 	## Minishell tests
 	ls | ls > ./test/minishell_output.txt
 	ls | cat >> ./test/minishell_output.txt
 	ls | cat | grep Make >> ./test/minishell_output.txt
+	
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
 	bash -c 'ls | ls; ls | cat; ls | cat | grep Make' > ./test/bash_output.txt
-	exitcode_bash=$?
 	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	exitcode_bash=$?
+	compare_output
 }
 
 env_tests() {
     echo "Running environment tests..."
+
 	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 exp_tests() {
     echo "Running expansion tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
+
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
-
+	compare_output
 }
 
 quote_tests() {
     echo "Running quotation tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 built_tests() {
     echo "Running built-in tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 assign_tests() {
     echo "Running assign tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 redirect_tests() {
     echo "Running redirect tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 heredoc_tests() {
     echo "Running here_doc tests..."
-		## Minishell tests
+
+	## Minishell tests
+	ls | ls > ./test/minishell_output.txt
 
 	exitcode_minishell=$?
 
-	echo -e "\tRunning Bash commands..."
  	## Bash tests
+	bash -c 'ls' > ./test/bash_output.txt
 
 	exitcode_bash=$?
-	
-	## Compare outputs and exit codes
-	echo -e "\tComparing output..."
-	diff ./test/bash_output.txt ./test/minishell_output.txt #> /dev/null
-	if [ "$?" -eq "0" ]; then
-    	echo -e "\t\e[32mOutput OK!\e[0m"
-	else
-    	echo -e "\t\e[31mOutput KO!\e[0m"
-	fi
-
-	## Compare exit codes
-	echo -e "\tComparing exit codes..."
-	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
-    	echo -e "\t\e[32mExit code OK!\e[0m\n"
-	else
-    	echo -e "\t\e[31mExit code KO!\e[0m"
-    	echo -e "\tExpected exit code: $exitcode_bash"
-    	echo -e "\tFound exit code: $exitcode_minishell\n"
-	fi
+	compare_output
 }
 
 if [ "$1" == "s" ]; then
@@ -323,16 +201,16 @@ elif [ "$1" == "exp" ]; then
 elif [ "$1" == "q" ]; then
     quot_tests
     exit 0
-elif [ "$1" == "buil" ]; then
+elif [ "$1" == "b" ]; then
     built_tests
     exit 0
-elif [ "$1" == "as" ]; then
+elif [ "$1" == "a" ]; then
     assign_tests
     exit 0
-elif [ "$1" == "re" ]; then
+elif [ "$1" == "r" ]; then
     redirect_tests
     exit 0
-elif [ "$1" == "here" ]; then
+elif [ "$1" == "h" ]; then
     heredoc_tests
     exit 0
 else
@@ -350,7 +228,7 @@ else
 	exit 0
 fi
 rm -rf ./test
-echo "iets"
+
 
 ###############################################################3
 
