3de40661b13f361e4d5fe9e02f730c4bd9eb657b
winglessOracle <carlo@wesseling.io>
2023-04-24 18:05:48 +0200

    added files for testing

--- tester/tester.sh
+++ tester/tester.sh
@@ -4,7 +4,7 @@
 
 ### parameters: 
 ## s >> single command tests
-## m >> multi command tests
+## p >> multi command tests
 ## env >> env tests
 ## exp >> expander tests
 ## q >> quote tests
@@ -12,6 +12,9 @@
 ## a >> assign tests
 ## r >> redirect tests
 ## h >> here_doc tests
+## sig >> signal tests
+## c >> conditional pipe tests
+## w >> wildcard tests
 
 ## make tmp dit and ensure that the temporary directory is always cleaned up.
 mkdir -p ./tester/output
@@ -40,7 +43,7 @@ compare_output() {
 	if [ "$exitcode_minishell" -eq "$exitcode_bash" ]; then
     	printf "\t\e[32mExit code OK!\e[0m\n"
 	else
-    	printf "\t\e[31mExit code KO!\e\n[0m"
+    	printf "\t\e[31mExit code KO!\e[0m\n"
     	printf "\tExpected exit_code: $exitcode_bash\n"
     	printf "\tFound exit_code: $exitcode_minishell\n"
 	fi
@@ -51,49 +54,56 @@ run_tests() {
 	printf "\nRunning $file_name...\n"
     printf "\nRunning $file_name...\n" >> ./trace/traces
 	## Minishell tests
+
 	while read line;
 	do 
-		$line > ./tester/output/minishell_output
-		exitcode_minishell=$?
+	eval "$line" > ./tester/output/minishell_output
+	exitcode_minishell=$?
 	## bash tests
-		bash -c $line > ./tester/output/bash_output
-		exitcode_bash=$?
-		printf "%s\n" $line
-		compare_output
+	bash -c "$line" > ./tester/output/bash_output
+	exitcode_bash=$?
+	compare_output
 	done < $file_name
 }
 
 if [ "$1" == "s" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
-elif [ "$1" == "m" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/simple_tests";		run_tests;
+elif [ "$1" == "p" ]; then
+	file_name="tester/tests/pipe_tests";		run_tests;
 elif [ "$1" == "env" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/env_tests";			run_tests;
 elif [ "$1" == "exp" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/exp_tests";			run_tests;
 elif [ "$1" == "q" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/quote_tests";		run_tests;
 elif [ "$1" == "b" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/built_tests";		run_tests;
 elif [ "$1" == "a" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/assign_tests";		run_tests;
 elif [ "$1" == "r" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/redirect_tests";	run_tests;
 elif [ "$1" == "h" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/here_doc_tests";	run_tests;
 elif [ "$1" == "sig" ]; then
-	file_name="tester/tests/simple_tests";	run_tests;
+	file_name="tester/tests/signal_tests";		run_tests;
+elif [ "$1" == "c" ]; then
+	file_name="tester/tests/signal_tests";		run_tests;
+elif [ "$1" == "w" ]; then
+	file_name="tester/tests/wildcard_tests";	run_tests;
 else
 	echo "Running all tests..."
-	file_name="tester/tests/simple_tests"; 	run_tests
-	file_name="tester/tests/simple_tests"; 	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
-	file_name="tester/tests/simple_tests";	run_tests
+	file_name="tester/tests/simple_tests"; 		run_tests
+	file_name="tester/tests/pipe_tests";	 	run_tests
+	file_name="tester/tests/env_tests";			run_tests
+	file_name="tester/tests/exp_tests";			run_tests
+	file_name="tester/tests/quote_tests";		run_tests
+	file_name="tester/tests/built_tests";		run_tests
+	file_name="tester/tests/assign_tests";		run_tests
+	file_name="tester/tests/redirect_tests";	run_tests
+	file_name="tester/tests/here_doc_tests";	run_tests
+	file_name="tester/tests/signal_tests";		run_tests
+	file_name="tester/tests/cond_pipe_tests";	run_tests
+	file_name="tester/tests/wildcard_tests";	run_tests
 	exit 0
 fi
-# rm -rf ./tester/test
+# rm -rf ./tester/output
