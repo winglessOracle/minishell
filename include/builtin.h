b5a25074c1ae51bf048cb04f3486183a0b9c0783
ccaljouw <ccaljouw@student.codam.nl>
2023-04-25 21:46:43 +0200

    norminette header files

--- include/builtin.h
+++ include/builtin.h
@@ -6,16 +6,15 @@
 /*   By: cariencaljouw <cariencaljouw@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 09:29:13 by cariencaljo   #+#    #+#                 */
-/*   Updated: 2023/04/23 17:38:20 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/04/25 21:43:10 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
-#include "minishell.h"
-#include <limits.h>
+# include "minishell.h"
+# include <limits.h>
 
 int		execute_echo(char **cmd_vector, t_node *env_list);
 int		execute_cd(char **cmd_vector, t_node *env_list);
