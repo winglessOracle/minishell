4d06270a5cf6c7225208de0ec5b6b76c4594e64e
ccaljouw <ccaljouw@student.codam.nl>
2023-05-03 10:16:50 +0200

    added syntax error check for redirects

--- src/utils/redirect_utils.c
+++ src/utils/redirect_utils.c
@@ -6,7 +6,7 @@
 /*   By: cariencaljouw <cariencaljouw@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/30 15:56:14 by cariencaljo   #+#    #+#                 */
-/*   Updated: 2023/05/03 10:06:58 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/05/03 10:15:51 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,34 +44,6 @@ int	get_redirect_type(t_node **tokens, t_smpl_cmd *cmd)
 	return (type);
 }
 
-// int	redirect_tokens(t_node **tokens, t_smpl_cmd *cmd, t_list *list)
-// {
-// 	int					state;
-// 	int					type;
-// 	static t_function	*parse[16];
-
-// 	(void)list;
-// 	parse[COMMENT] = remove_comment;
-// 	parse[SQUOTE] = merge_quoted;
-// 	parse[DQUOTE] = merge_quoted;
-// 	parse[EXPAND] = expand;
-// 	parse[TILDE] = expand_tilde;
-// 	state = get_redirect_type(tokens, cmd);
-// 	if (state == -1)
-// 		return (syntax_error(tokens, cmd, "Redirect\n", -1));
-// 	type = check_token_content(*tokens, WORD);
-// 	if (type == COMMENT)
-// 		type = parse[type](tokens, cmd);
-// 	if (!*tokens || (*tokens)->type == PIPE_END)
-// 		return (syntax_error(tokens, cmd, "Redirect\n", -1));
-// 	type = check_token_content(*tokens, WORD);
-// 	(*tokens)->type = state;
-// 	if (type != WORD && type != ASSIGN && !(state == HEREDOC && type == EXPAND))
-// 		type = parse[type](tokens, cmd);
-// 	lstadd_back(&cmd->redirect, lstpop(tokens));
-// 	return (syntax_error(tokens, cmd, "Redirect\n", type));
-// }
-
 int	redirect_tokens(t_node **tokens, t_smpl_cmd *cmd, t_list *list)
 {
 	int					state;
@@ -86,6 +58,8 @@ int	redirect_tokens(t_node **tokens, t_smpl_cmd *cmd, t_list *list)
 	state = get_redirect_type(tokens, cmd);
 	if (state == -1)
 		return (syntax_error(tokens, cmd, "Redirect syntax error\n", -1));
+	if ((*tokens)->type == PIPE_END || (*tokens)->type == AND || (*tokens)->type == OR)
+			return (syntax_error(tokens, cmd, "Redirect syntax error\n", -1));
 	(*tokens)->type = check_token_content(*tokens, WORD);
 	if (state == HEREDOC && ((*tokens)->type == SQUOTE || (*tokens)->type == DQUOTE))
 		state = HEREDOCQ;
