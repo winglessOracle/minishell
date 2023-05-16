25d47bf660bfd8e1ceab18470a69adb037667515
Carien Caljouw <cariencaljouw@Cariens-Air.home>
2023-05-16 19:51:23 +0200

    fix for unset segfault (still needs significant change to built cmds to make norminette proof)

--- builtin/unset.c
+++ builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: cariencaljouw <cariencaljouw@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/16 20:02:30 by cariencaljo   #+#    #+#                 */
-/*   Updated: 2023/05/11 17:39:13 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/05/16 19:45:22 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,54 +53,60 @@ int	check_valid_unset(char *str)
 	return (0);
 }
 
-int	remove_var(t_node *temp, t_node *prev, char *name)
+void	remove_var(t_node **token, t_node *prev, char *name)
 {
-	if (ft_isalpha(name[0]) || name[0] == '_')
-	{
-		prev->next = temp->next;
-		remove_node(&temp, NULL);
-		free(name);
-		return (1);
-	}
-	return (0);
+	t_node	*temp;
+	
+	temp = *token;
+	*token = (*token)->next;
+	if (prev && *token)
+		prev->next = *token;
+	lstdelone(temp, delete_content);
+	free(name);
 }
 
-int	check_and_remove(t_node **temp, char *str, t_node **prev)
+int	check_and_remove(t_node **token, char *str)
 {
 	char	*name;
+	t_node	*prev;
 
-	name = get_name((*temp)->content);
+	name = get_name((*token)->content);
+	prev = NULL;
 	if (!ft_strcmp(str, name))
 	{
-		if (remove_var(*temp, *prev, name))
+		if ((ft_isalpha(name[0]) || name[0] == '_'))
+		{
+			remove_var(token, prev, name);
 			return (1);
+		}
+		else
+			return (0);
 	}
 	else
 	{
-		*prev = *temp;
-		*temp = (*temp)->next;
+		prev = *token;
+		*token = (*token)->next;
 	}
 	free(name);
 	return (0);
 }
 
-int	execute_unset(char **cmd_vector, t_node *env_list)
+int	execute_unset(char **cmd_vector, t_node **env_list)
 {
 	int		i;
 	int		check;
 	t_node	*temp;
-	t_node	*prev;
 
 	i = 1;
 	while (cmd_vector[i])
 	{
-		temp = env_list;
+		temp = *env_list;
 		check = check_valid_unset(cmd_vector[i]);
 		if (check)
 			return (check);
 		while (temp && temp->content)
 		{
-			if (check_and_remove(&temp, cmd_vector[i], &prev))
+			if (check_and_remove(&temp, cmd_vector[i]))
 				break ;
 		}
 		i++;
--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 /*   By: cariencaljouw <cariencaljouw@student.co      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/04/13 09:29:13 by cariencaljo   #+#    #+#                 */
-/*   Updated: 2023/05/11 19:59:49 by cariencaljo   ########   odam.nl         */
+/*   Updated: 2023/05/16 19:23:39 by cariencaljo   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 int		execute_echo(char **cmd_vector, t_node *env_list);
 int		execute_cd(char **cmd_vector, t_node *env_list);
 int		execute_pwd(char **cmd_vector, t_node *env_list);
-int		execute_unset(char **cmd_vector, t_node *env_list);
+int		execute_unset(char **cmd_vector, t_node **env_list);
 int		execute_export(char **cmd_vector, t_node *env_list);
 int		execute_exit(char **cmd_vector, t_node *env_list);
 int		execute_env(char **cmd_vector, t_node	*env_list);
